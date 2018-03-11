5b6aa2809016be2fa5c71c6905efe2f941cfa90d
Luciano Miguel <luciano_limamiguel@hotmail.com>
2018-03-11 15:44:38 -0300

    Projeto criado

---
+++ src/main/java/com/luciano/rumojr/CursomcApplication.java
@@ -0,0 +1,12 @@
+package com.luciano.rumojr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+}
